56aea818be87df13ba59ca5950d35073c2d3e415
GABRIEL PEREIRA FERREIRA <gabriel.pferreira7@senacsp.edu.br>
2022-09-22 21:37:03 -0300

    adicionando listagem e login

---
+++ criarProcessamento.php
@@ -0,0 +1,32 @@
+<?php
+            //dados para conexão ao mysql
+        $mysqlhostname = '144.22.244.104';
+        $mysqlport = '3306';
+        $mysqlusername = 'Bravo4Fun';
+        $mysqlpassword = 'Bravo4Fun';
+        $mysqldatabase = 'Bravo4Fun';
+
+        //Mostra a String de Conexao ao Mysql
+
+        $dsn = 'mysql:host=' . $mysqlhostname .';dbname='. $mysqldatabase . ';port='. $mysqlport;
+        $pdo = new PDO($dsn, $mysqlusername, $mysqlpassword);
+
+        //Capturar os valores das variaveis
+        $nome = $_POST['nome'];
+        $email = $_POST["email"];
+        $senha = $_POST["senha"];
+
+        //Monta o comando de inserção
+        $cmdtext = "INSERT INTO ADMINISTRADOR(ADM_NOME, ADM_EMAIL, ADM_SENHA) VALUES('". $nome ."','". $email ."','". $senha . "')";
+        $cmd = $pdo->prepare($cmdtext);
+
+        //Executa o comando e verifica se teve sucesso
+        $status = $cmd->execute();
+        if ($status) {
+            echo "Criação do Administrador com sucesso";
+        }else {
+            echo "Ocorreu um erro ao inserir";
+        };
+    ?>
+</body>
+</html>
---
+++ login.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
+    <link rel="stylesheet" href="style1.css">
+
+    <title>Login - Admin</title>
+</head>
+<body>
+    <div class="main-login">
+        <div class="left-login">
+            <img src="images/logo-dark-2.png" alt="Logo Bravo4Fun">
+            <h1>Faça seu login<br>E controle sua plataforma de forma simples</h1>
+        </div>
+        <div class="right-login">
+            <div class="card-login">
+                <h1>LOGIN</h1>
+                <form Action="index.php" method="POST">
+                    <div class="textfield">
+                        <label for="email"> Email </label>
+                        <input type="email" name="email" id="email" placeholder="Email" required>
+                    </div>
+                    <div class="textfield">
+                        <label for="senha"> Senha </label>
+                        <input type="password" name="senha" placeholder="Senha" required>
+                    </div>
+                    <input type="submit" class="btn-login" value="Login">
+                </form>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
